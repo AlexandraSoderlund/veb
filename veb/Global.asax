2e24bbd34a8156f904cf49bb3f2ae69187e84bb0
AlexandraSoderlund <43433913+AlexandraSoderlund@users.noreply.github.com>
2018-12-20 19:46:24 +0100

    Add project files.

---
+++ veb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="veb.MvcApplication" Language="C#" %>
---
+++ veb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(veb.Startup))]
+namespace veb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
