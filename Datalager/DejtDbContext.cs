6c6993d252fd463c858400a4269baafe91793776
sufo6969 <43465892+sufo6969@users.noreply.github.com>
2018-12-27 11:36:25 +0100

    tog bort bort dbcontextklass

--- Datalager/DejtDbContext.cs
+++ Datalager/DejtDbContext.cs
@@ -6,7 +6,7 @@ namespace webapp.Models
     public class DejtDbContext : DbContext
     {
         public DbSet<Profile> Profiles { get; set; }
-
+      
 
         public DejtDbContext() : base("karleksmums") {
 
