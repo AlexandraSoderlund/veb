780e597f711b675437c26193c5f32680f0e08a82
elinto <elin_torebrand94@hotmail.com>
2019-01-02 14:54:14 +0100

    lite validering o grejer inte klart

--- webapp/Models/EditProfileViewModel.cs
+++ webapp/Models/EditProfileViewModel.cs
@@ -1,13 +1,20 @@
-﻿using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web;
 
 namespace webapp.Models
 {
     public class EditProfileViewModel
     {
+        [Required(ErrorMessage ="Du måste fylla i en beskrivning")]
+        [StringLength(100, ErrorMessage ="Beskrivningen måste innehålla minst 2 bokstäver", MinimumLength =2)]
         public string Description { get; set; }
         public HttpPostedFileBase ProfileImage { get; set; }
         public string ProfileImageUrl { get; set; }
+        [Required(ErrorMessage = "Du måste fylla i en favoritkaka ")]
+        [StringLength(100, ErrorMessage = "Favoritkakan måste innehålla minst 2 bokstäver", MinimumLength = 2)]
         public string Favoritkaka { get; set; }
+        [Required(ErrorMessage = "Du måste fylla i ett namn")]
+        [StringLength(100, ErrorMessage = "Namnet måste innehålla minst 2 bokstäver", MinimumLength = 2)]
         public string Namn { get; set; }
 
     }
