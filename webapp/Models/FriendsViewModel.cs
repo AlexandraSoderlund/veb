dc186885db33ab772c87b01533ac20463b7dc344
AlexandraSoderlund <43433913+AlexandraSoderlund@users.noreply.github.com>
2019-01-03 14:00:18 +0100

    validering

--- webapp/Models/FriendsViewModel.cs
+++ webapp/Models/FriendsViewModel.cs
@@ -15,7 +15,7 @@ namespace webapp.Models
         }
         [Required(ErrorMessage ="Du måste fylla i en favoritkaka")]
         [StringLength(40,ErrorMessage ="Skriv minst 3-40 bokstäver", MinimumLength = 3)]
-        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Använd endast bokstäver")]
+        [RegularExpression(@"^([a-åäöA-ÅÄÖ \.\&\'\-]+)$", ErrorMessage = "Använd endast bokstäver")]
         public string SearchText { get; set; }
         public List<Profile> Profiles { get; set; }
     }
